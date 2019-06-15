3c997360b48edbce7855561f9780870b150c912d
Richard Curteis <richardcurteis@users.noreply.github.com>
2019-06-15 22:12:22 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 
-infile=$0
+path=`pwd`/$1
+cmd=$2
 
-while IFS= read -r line
-do
-  git clone $line
-done
+function clone() {
+	while IFS= read -r line
+	do
+        git clone $line
+	done < $path
+}
+
+function pull() {
+	cd /opt
+	for D in `find . -type d`
+	do
+		cd $D
+		if [ -d .git ]; then
+			git pull
+		fi 
+	done
+}
+
+cd /opt
+if [ $2 -eq "clone"]
+then
+	clone
+
+elif [ $2 -eq "pull"]
+then
+	pull
+else
+	echo "Valid arguments are 'pull' or 'clone'"
+fi
