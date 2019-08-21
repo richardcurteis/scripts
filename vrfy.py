b70402c40791c99cf739fd0e182db5546a4bbf53
3therk1ll <richardcurteis@users.noreply.github.com>
2019-08-22 00:50:47 +0100

    Update vrfy.py

--- vrfy.py
+++ vrfy.py
@@ -11,7 +11,7 @@ for name in open(sys.argv[1]):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         connect = s.connect((ip, 25))
         banner = s.recv(1024)
-        print banners.send('VRFY ' + name + '\r\n')
+        print banner.send('VRFY ' + name + '\r\n')
         results = s.recv(1024)
         print results
         s.close()
