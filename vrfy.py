d221cc96120ae23a109c0f2fdc216b2194adea5f
3therk1ll <richardcurteis@users.noreply.github.com>
2019-08-22 00:50:14 +0100

    Update vrfy.py

--- vrfy.py
+++ vrfy.py
@@ -7,9 +7,8 @@ if len(sys.argv) != 2:
     print "Usage: vrfy.py <user_list> <ip_list>"
 
 for name in open(sys.argv[1]):
-    print name
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     for ip in open(sys.argv[2]):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         connect = s.connect((ip, 25))
         banner = s.recv(1024)
         print banners.send('VRFY ' + name + '\r\n')
