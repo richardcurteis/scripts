f860b334e5e7be041840d7ecf3cb9fd5b99c518d
3therk1ll <richardcurteis@users.noreply.github.com>
2019-08-22 00:37:47 +0100

    Create vrfy.py

---
+++ vrfy.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import socket 
+import sys
+
+if len(sys.argv) != 2:
+    print "Usage: vrfy.py <user_list> <ip_list>"
+
+for name in open(sys.argv[1]):
+    print name
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    for ip in open(sys.argv[2]):
+        connect = s.connect((ip, 25))
+        banner = s.recv(1024)
+        print banners.send('VRFY ' + name + '\r\n')
+        results = s.recv(1024)
+        print results
+        s.close()
