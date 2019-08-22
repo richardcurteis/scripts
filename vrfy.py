7f9c7d6aabbd921f27ea87e058f6294e9d4b4af3
3therk1ll <richardcurteis@users.noreply.github.com>
2019-08-22 01:15:53 +0100

    Update vrfy.py

--- vrfy.py
+++ vrfy.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python
 
-import socket 
+import socket
 import sys
 
 if len(sys.argv) != 2:
     print "Usage: vrfy.py <ip_list> <user_list>"
 
-for ip in open(sys.argv[1]):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    connect = s.connect((ip, 25))
-    for name in open(sys.argv[2]):
-        banner = s.recv(1024)
-        print banner.send('VRFY ' + name + '\r\n')
-        results = s.recv(1024)
-        print results
-    s.close()
+with open(sys.argv[0]) as ip_list, open(sys.argv[1]) as name_list:
+    for ip in ip_list:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        connect = s.connect((ip, 25))
+        for name in name_list:
+            banner = s.recv(1024)
+            print banner.send('VRFY ' + name + '\r\n')
+            results = s.recv(1024)
+            print results
+        s.close()
