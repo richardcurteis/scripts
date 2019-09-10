94a91c3d18a5ed9ff499933b13ccc0c779232bb2
3therk1ll <richardcurteis@users.noreply.github.com>
2019-09-11 00:26:53 +0100

    Update windows-enum.bat

--- windows-enum.bat
+++ windows-enum.bat
@@ -1,51 +1,66 @@
 echo "Windows Enum Script for Windows Enum"
 :: http://www.fuzzysecurity.com/tutorials/16.html
 :: https://it-ovid.blogspot.com/2012/02/windows-privilege-escalation.html?m=1
-
-echo "--- Hostname ---" > report.txt
+echo "" > report.txt
+echo "--- Hostname ---" >> report.txt
 hostname  >> report.txt
+echo "" >> report.txt
 
 echo "-- OS Version --"  >> report.txt
 systeminfo | findstr /B /C:"OS Name" /C:"OS Version" >> report.txt
+echo "" >> report.txt
 
 echo "--- Users ---" >> report.txt
 net users >> report.txt
+echo "" >> report.txt
 
 echo "-- System Info --" >> report.txt
 systeminfo >> report.txt
+echo "" >> report.txt
 
 echo "-- Active Network Connections --"  >> report.txt
 netstat -ano  >> report.txt
+echo "" >> report.txt
 
 echo "-- Firewall State --"  >> report.txt
 netsh firewall show state >> report.txt
+echo "" >> report.txt
 
 echo "-- Firewall Config --" >> report.txt
 netsh firewall show config >> report.txt
+echo "" >> report.txt
 
 echo "-- Scheduled Tasks --" >> report.txt
 schtasks /query /fo LIST /v >> report.txt
+echo "" >> report.txt
 
 echo "-- Process and Linked Services -- " >> report.txt
 tasklist /SVC >> report.txt
+echo "" >> report.txt
 
 echo "-- Startup Services --" >> report.txt
 net start >> report.txt
+echo "" >> report.txt
 
 echo "-- Check for Installed Drivers ---" >> report.txt
 DRIVERQUERY >> report.txt
+echo "" >> report.txt
 
 echo "-- Check for Installed Patches ---" >> report.txt
 wmic qfe get Caption,Description,HotFixID,InstalledOn >> report.txt
+echo "" >> report.txt
 
 echo "-- Check for Installed Patches ---" >> report.txt
 wmic qfe get Caption,Description,HotFixID,InstalledOn | findstr /C:"KB.." /C:"KB.." >> report.txt
+echo "" >> report.txt
 
 echo "--- Searching for  files that contain 'password' in filename --" >> report.txt
 dir /s *password* >> report.txt
+echo "" >> report.txt
 
 echo "--- Searching for  files that contain 'password' with .txt extension --" >> report.txt
 findstr /si password *.txt >> report.txt
+echo "" >> report.txt
 
 echo "--- Search for Interesting XML --" >> report.txt
 dir /s Groups.xml >> report.txt
@@ -54,15 +69,18 @@ dir /s Printers.xml >> report.txt
 dir /s ScheduledTasks.xml >> report.txt
 dir /s Drives.xml >> report.txt
 dir /s DataSources.xml >> report.txt
+echo "" >> report.txt
 
 echo "--- Find autostart files with unquoted servive path --" >> report.txt
 wmic service get name,displayname,pathname,startmode |findstr /i "Auto" |findstr /i /v "C:\\\" |findstr /i /v """  >> report.txt
+echo "" >> report.txt
 
 echo "--- Check for AlwaysInstallElevated ---" >> report.txt
 echo "HKCU" >> report.txt
-reg query HKCU\SOFTWARE\Policies\Microsoft\Windows\Installer /v AlwaysInstallElevated >> report.txt
+reg query HKCU\SOFTWARE\Policies\Microsoft\Windows\Installer /v AlwaysInstallElevated >> report.txt 
 echo "HKLM" >> report.txt
 reg query HKLM\SOFTWARE\Policies\Microsoft\Windows\Installer /v AlwaysInstallElevated >> report.txt
+echo "" >> report.txt
 
 echo "--- Search Registry for 'password' ---" >> report.txt
 reg query HKLM /f password /t REG_SZ /s >> report.txt
@@ -71,13 +89,15 @@ reg query "HKCU\Software\ORL\WinVNC3\Password" >> report.txt
 reg query "HKLM\SOFTWARE\Microsoft\Windows NT\Currentversion\Winlogon" >> report.txt
 reg query "HKLM\SYSTEM\Current\ControlSet\Services\SNMP" >> report.txt
 reg query" HKCU\Software\SimonTatham\PuTTY\Sessions" >> report.txt
-
+echo "" >> report.txt
 
 echo "--- Search Everywhere for files containing 'password'> *.xml *.ini *.txt ---" >> report.txt
 findstr /si password *.xml *.ini *.txt *.xls *.xlsx  >> report.txt
+echo "" >> report.txt
 
 echo "WMI Hotfixes" >> report.txt
 wmic qfe list full >> report.txt
+echo "" >> report.txt
 
 echo "---- Networking ---" >> report.txt
 ipconfig /all >> report.txt
