9c93f7826ec24117689ef77dbb5c0ab2c54a77e6
3therk1ll <richardcurteis@users.noreply.github.com>
2019-09-12 17:40:39 +0100

    Update and rename chisel.sh to chisel_update.sh

--- chisel_update.sh
+++ chisel_update.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/bash
+
 # Helper script to download fresh version of Chisel and build a shrunk binary.
+
 echo "[!] Deleting previous chisel folder and binaries..."
 /usr/bin/rm -rf /opt/chisel
 /usr/bin/cd /opt
+
 echo "[!] Creating fresh chisel clone in '/opt'..."
 git clone https://github.com/jpillora/chisel.git
 /usr/bin/cd chisel
+
 echo "[!] Building new chisel Go binary without debug information..."
 /usr/local/go/bin/go build -ldflags="-s -w"
+
 echo "[!] Packing with upx..."
 /usr/bin/upx brute chisel
