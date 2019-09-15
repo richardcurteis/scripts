29f0082974724ccc04f9964e501889bad466f27c
3therk1ll <richardcurteis@users.noreply.github.com>
2019-09-15 22:22:00 +0100

    Update nmap.sh

--- nmap.sh
+++ nmap.sh
@@ -9,13 +9,11 @@ ip=${echo $host | grep ":" | cut -d ":" -f 2}
 hostname=${echo $host | grep "." | cut -d "." -f 1}
 do
   if [ ! -d $ip ]; then
-    $mkdir $hostname
-    $mkdir $hostname/nmap
-    $mkdir $hostname/nmap/udp
+    $mkdir -p $hostname/nmap/udp
 
     # Nmap 
-    $nmap -sS-sC -sV --version-all -A -O -p- -oA $hostname/nmap/initial $ip
-    $nmap -sU -sC -sV --version-all -p- -oA $hostname/nmap/udp/initial_udp $ip
+    $nmap -sS -sC -sV --version-all -A -O -p- -oA $hostname/nmap/initial $ip
+    $nmap -sU -sV  -F -oA $hostname/nmap/udp/common_udp_ports $ip
   fi
 # End of loop
 done
